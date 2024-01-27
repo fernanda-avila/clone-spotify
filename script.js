1275165527b7b03bfbc2a3f1a9d64cc818e5fefc
Fernanda Ávila <153337144+fernanda-avila@users.noreply.github.com>
2024-01-26 23:32:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
         .then((response) => response.json())
         .then((result) => displayResults(result))
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
+    resultPlaylist.classList.add("hidden");
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
 
